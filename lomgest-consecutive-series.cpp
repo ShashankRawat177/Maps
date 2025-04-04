5c9c5e518a4d1a7216aa980d38974b6a800dceed
ShashankRawat177 <shashankrawat177@gmail.com>
2025-04-04 13:44:47 +0530

    Update lomgest-consecutive-series.cpp

--- lomgest-consecutive-series.cpp
+++ lomgest-consecutive-series.cpp
@@ -1,1 +1,25 @@
-
+class Solution {
+    public:
+        int longestConsecutive(vector<int>& nums) {
+            if (nums.empty()) return 0;
+    
+        set<int> s(nums.begin(), nums.end());  
+        int maxLength = 0, currentStreak = 0;
+    
+        for (int num : s) {
+            if (s.find(num - 1) == s.end()) {
+                int currentNum = num;
+                currentStreak = 1;
+                
+                while (s.find(currentNum + 1) != s.end()) {
+                    currentNum++;
+                    currentStreak++;
+                }
+    
+                maxLength = max(maxLength, currentStreak);
+            }
+        }
+        
+        return maxLength;
+        }
+    };
